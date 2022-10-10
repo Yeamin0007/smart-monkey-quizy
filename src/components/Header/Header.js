1b883d4b20950ee6d4542b82c35c32cc852c6a33
Yeamin0007 <yh.hossainsaaz@gmail.com>
2022-10-11 04:47:48 +0600

    commit header section

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,19 +2,24 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            <div>
+        <div className='flex justify-center items-center my-28'>
+        
+
+
+
+            
+            <div className= 'text-6xl'>
+            <img className='px-60' src='https://smartmonkey.ca/wp-content/uploads/2020/11/Smart-Monkey-Logo-v2-square.png' alt='smart-monkey'></img>
 
                 <h1>
-                    Time to surpress your own limit, right here right now.
+                    <span className='text-amber-500'>Time to surpress your own limit,</span> right here, right now.
                 </h1>
 
             </div>
-            <div>
-             <img></img>
+        
 
+            
             </div>
-        </div>
     );
 };
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Header from '../Header/Header';
 import Topic from '../Topics/Topic';
 
 const Home = () => {
     const topics = useLoaderData();
     return (
-        <div className='flex justify-center'>
+        <div>
             <div>
+                <Header></Header>
 
             </div>
 
 
+        <div className='flex justify-center'>
         <div className='grid grid-cols-2 gap-28 w-1/2 m-20'>
         {
          topics.data.map(topic => <Topic
@@ -19,6 +22,7 @@ const Home = () => {
          ></Topic>)
          }
 
+        </div>
         </div>
         </div>
     );
